36b16440a61b007cbab6b4b0157c6f35eb4ac317
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-12 13:35:13 +0100

    ajax jquery finito

--- js/main.js
+++ js/main.js
@@ -1,2 +1,45 @@
 'use strict';
-	
+$(document).ready(function() {	
+	$('#pobierz-dane').click(function(event) {
+		event.preventDefault();
+
+		//pierwszy sposób jQuery
+
+		$.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function (data) {
+			console.log(data);
+		});
+
+
+			// drugi sposób jQuey
+		//	$.ajax({
+		//		 url: "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl",
+		//		 dataType: 'json',
+		//		 success: function (resultJSON) {
+		//
+		//			 /*
+		//			 !!! Nie parsujemy do postaci obiektu JSON, bo od razu
+		//			 taki obiekt dostajemy w odpowiedzi !!!
+		//			  var jsonObj = JSON.parse(resultJSON);
+		//			 */
+		//
+		//			 console.log(resultJSON);
+		//
+		//			 var pUserId = document.createElement('p');
+		//			 var pUserName = document.createElement('p');
+		//			 var pUserURL = document.createElement('p');
+		//
+		//			 pUserId.innerHTML = "User ID: " + resultJSON.userId;
+		//			 pUserName.innerHTML = "User Name: " + resultJSON.userName;
+		//			 pUserURL.innerHTML = "User URL: http://" + resultJSON.userURL;
+		//
+		//			 document.body.appendChild(pUserId);
+		//			 document.body.appendChild(pUserName);
+		//			 document.body.appendChild(pUserURL);
+		//		  },
+		//		  onerror: function (msg) {
+		//			  console.log(msg);
+		//      }
+		//   });
+		});
+
+});
